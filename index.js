06d631d5b100fdb4171bb1c6936a536a5ec0394d
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-11-07 19:52:50 +0300

    get all service

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use([cors(), express.json()]);
+
+const uri = process.env.MONGO_URL;
+console.log(uri);
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+    try {
+        const serviceCollection = client
+            .db("foodService")
+            .collection("services");
+
+        // get all services
+        app.get("/services", async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+        
+    } finally {
+    }
+};
+
+run().catch((error) => console.log(error));
+
+app.get("/", (req, res) => {
+    res.send("Cloud Kitchen server is running");
+});
+
+app.listen(port, () => {
+    console.log(`Cloud Kitchen server is running on ${port}`);
+});
