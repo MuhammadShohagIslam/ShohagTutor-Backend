b64d33ec7d4e3964fb330e71d53eafd153312c98
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2022-11-10 10:47:02 +0300

    added try catch into middleware

--- index.js
+++ index.js
@@ -20,20 +20,30 @@ const client = new MongoClient(uri, {
 
 // verify user by JWT
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send({ message: "unauthorize access" });
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SCREAT, function (err, decoded) {
-        if (err) {
-            return res.status(403).send({ message: "Forbidden Access" });
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader) {
+            return res.status(401).send({ message: "unauthorize access" });
         }
-        req.decoded = decoded;
-        next();
-    });
+
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(
+            token,
+            process.env.ACCESS_TOKEN_SCREAT,
+            function (err, decoded) {
+                if (err) {
+                    return res
+                        .status(403)
+                        .send({ message: "Forbidden Access" });
+                }
+                req.decoded = decoded;
+                next();
+            }
+        );
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
 };
 
 const run = async () => {
